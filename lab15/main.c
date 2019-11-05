a834604650cf4d94fc93dfc749dfb92a26220f7f
macneil.evan <macneil.evan@csa3.cs.ucalgary.ca>
2019-11-05 12:27:34 -0700

    Fix some errors. Add some comments.

--- lab15/main.c
+++ lab15/main.c
@@ -1,15 +1,21 @@
-// Example of nested structures.
+/* Example of nested structures.
+ *
+ * This file defines a structure 'rect' representing a rectangle, defined by a
+ * point, some dimensions, and a color. The main function declares a rect,
+ * assigns it some values, then prints out its color formatted as a hex code.
+ */
 
-#include <stdint.h>
-#include <stdio.h>
+#include <stdint.h> // Defines int32_t, uint32_t, uint8_t
+#include <stdio.h>  // Defines printf
 
-#define DEFAULT_R 0
-#define DEFAULT_G 0
-#define DEFAULT_B 0
+#define DEFAULT_R 1
+#define DEFAULT_G 2
+#define DEFAULT_B 3
 #define DEFAULT_A 255
 
+// Declare some structures
 struct point2d {
-  uint32_t x, y;
+  int32_t x, y;
 };
 
 struct size2d {
@@ -20,6 +26,7 @@ struct color4d {
   uint8_t r, g, b, a;
 };
 
+// Declare a rectangle structure, made up of nested structures.
 struct rect {
   struct point2d point;
   struct size2d  size;
@@ -28,31 +35,35 @@ struct rect {
 
 
 
-struct rect new_rect(int x, int y, int w, int h);
+// Function prototypes. These functions are defined below.
+struct rect new_rect(int32_t x, int32_t y, uint32_t w, uint32_t h);
 void print_rect_color(struct rect * r);
 
 
 
 int main(int argc, char * argv[])
 {
-  struct rect r;
+  struct rect r;                // Declare a new rectangle
 
-  r = new_rect(1, 2, 3, 4);
-  print_rect_color(&r);
+  r = new_rect(-1, 2, 3, 4);     // Assign some values to it
+  print_rect_color(&r);         // Print out its color
 
-  return 0;
+  return 0;                     // Exit
 }
 
 
 
-struct rect new_rect(int x, int y, int w, int h)
+struct rect new_rect(int32_t x, int32_t y, uint32_t w, uint32_t h)
 {
   struct rect r;
 
+  // Fill in its position and dimensions based on function arguments
   r.point.x = x;
   r.point.y = y;
   r.size.w = w;
   r.size.h = h;
+
+  // Fill in its color based on program defaults
   r.color.r = DEFAULT_R;
   r.color.g = DEFAULT_G;
   r.color.b = DEFAULT_B;
@@ -65,6 +76,8 @@ struct rect new_rect(int x, int y, int w, int h)
 
 void print_rect_color(struct rect * r)
 {
+  // Print out the rectangle's color in the format #aabbccdd, where, e.g.
+  // aa is the rectangle's red value in hexadecimal.
   printf("#%02hhx%02hhx%02hhx%02hhx\n", r->color.r, r->color.g, r->color.b, r->color.a);
 }
 
--- lab15/main.s
+++ lab15/main.s
@@ -3,9 +3,9 @@ fmt:    .string "#%02hhx%02hhx%02hhx%02hhx\n"
         .global main
         .balign 4
 
-DEFAULT_R = 0
-DEFAULT_G = 0
-DEFAULT_B = 0
+DEFAULT_R = 1
+DEFAULT_G = 2
+DEFAULT_B = 3
 DEFAULT_A = 255
 
 point2d_x = 0
@@ -35,9 +35,21 @@ main :
         stp     fp, lr, [sp, main_alloc]!
         mov     fp, sp
 
+        // Call new_rect(-1, 2, 3, 4), creating a new rectangle with values
+        //      (x, y, w, h) = (-1, 2, 3, 4)
+        // and with default values for (r, g, b, a).
+        // We must also compute the address at which this rectangle will be stored before
+        // calling new_rect.
+        mov     w0, -1
+        mov     w1, 2
+        mov     w2, 3
+        mov     w3, 4
         add     x8, fp, rect_s
         bl      new_rect
         
+        // Compute the address of the rectangle again and place it in X0, to give it as an
+        // argument to print_rect_color. (We could just do 'mov X0, X8' since the address was
+        // already in X8, but this is unsafe since new_rect is not guaranteed to preserve X8.)
         add     x0, fp, rect_s
         bl      print_rect_color
 
@@ -47,11 +59,15 @@ main :
 
 
 
-// This is a leaf function
+// This is a leaf function so there is no need to back up FP and LR.
+// It uses no local variables, so it does not need to allocate more stack space.
 new_rect :
-        add     x9, x8, rect_point
-        str     w0, [x9, point2d_x]
-        str     w1, [x9, point2d_y]
+        // add     x9, x8, rect_point
+        // str     w0, [x9, point2d_x]
+        // str     w1, [x9, point2d_y]
+
+        str     w0, [x8, rect_point + point2d_x]
+        str     w1, [x8, rect_point + point2d_y]
         
         add     x9, x8, rect_size
         str     w2, [x9, size2d_w]
